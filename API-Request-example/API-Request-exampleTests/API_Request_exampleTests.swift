25f8719536161a13105c7e6d766fce5b4424a5db
Fernando <f.c.gorris@gmail.com>
2026-04-01 20:01:29 +0200

    Initial commit

---
+++ API-Request-example/API-Request-exampleTests/API_Request_exampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  API_Request_exampleTests.swift
+//  API-Request-exampleTests
+//
+//  Created by Fernando Corral on 1/4/26.
+//
+
+import Testing
+
+struct API_Request_exampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
